f2f7f1a36cb9f634eb93e4dd8b3c66e323cbbab4
nntrn <17685332+nntrn@users.noreply.github.com>
2024-03-10 15:53:04 -0500

    Update for issues

--- scripts/build2.sh
+++ scripts/build2.sh
@@ -12,8 +12,8 @@ _log() { echo -e "\033[0;${2:-33}m$*\033[0m" 3>&2 2>&1 >&3 3>&-; }
 
 ghcurl() {
   local URL="${1:-"https://api.github.com/repos/nntrn/save/issues?per_page=100"}"
-  curl -L -s -o ${2:-/dev/stdout} -H "Authorization: Bearer $GITHUB_TOKEN" "$URL" --fail
-  [[ $? -ne 0 ]] && exit 1
+  curl -s -o ${2:-/dev/stdout} -H "Authorization: Bearer $GITHUB_TOKEN" "$URL" --fail
+  [[ $? -ne 0 ]] && return 1
 }
 
 jq_filter() {
@@ -52,6 +52,7 @@ download_artifacts() {
   _log "Running download_artifacts"
   ARTIFACTS_FILE=artifacts.json
   ghcurl "https://api.github.com/repos/nntrn/save/actions/artifacts" >$ARTIFACTS_FILE
+  curl -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/nntrn/save/actions/artifacts" >$ARTIFACTS_FILE
 
   LAST_ARCHIVE_DATA_URL="$(
     jq -r '(.artifacts|sort_by(.created_at)|map(select(.name == "page_data" and (.expired|not)).archive_download_url)|last)? // ""' $ARTIFACTS_FILE
@@ -59,45 +60,53 @@ download_artifacts() {
 
   if [[ -n $LAST_ARCHIVE_DATA_URL ]]; then
     _log "$LAST_ARCHIVE_DATA_URL"
-    ZIPFILENAME=$TMPDIR/data-$RANDOM.zip
+    ZIPFILENAME=data.zip
     curl -o $ZIPFILENAME -L -H "Authorization: Bearer $GITHUB_TOKEN" "$LAST_ARCHIVE_DATA_URL"
-    unzip -n -d _data $ZIPFILENAME
+    if [[ -d _data ]]; then
+      ls -laptr _data
+      mv _data _data~
+    fi
+    _log "Unzipping"
+    unzip -d _data $ZIPFILENAME
   else
+    cat $ARTIFACTS_FILE
+    _log "Archive data null"
     build_all
   fi
 }
 
 download_single_issue() {
-  local issueid=$1
+  issueid=$1
   _log "Running download_single_issue $issueid"
   ISSUEFILE=_data/body/${issueid}.json
   COMMENTSFILE=_data/comments/${issueid}.json
   mkdir -p _data/{body,comments}
 
-  ghcurl "https://api.github.com/repos/nntrn/save/issues/$issueid" | jq '.number |= tostring|del(.reactions,.user)' >$ISSUEFILE
+  ghcurl "https://api.github.com/repos/nntrn/save/issues/${issueid}" $ISSUEFILE
   NUMCOMMENTS=$(jq -r '.comments' $ISSUEFILE)
   if [[ $NUMCOMMENTS -gt 0 ]]; then
     _log "Writing $COMMENTSFILE"
-    ghcurl "https://api.github.com/repos/nntrn/save/issues/$issueid/comments?per_page=100" | jq_filter >$COMMENTSFILE
+    ghcurl "https://api.github.com/repos/nntrn/save/issues/$issueid/comments?per_page=100" >$COMMENTSFILE
   fi
 }
 
 trap 'rm -rf "$TMPDIR"' EXIT
 
-echo "$@"
-run_command=""
+run_command="build_all"
+
 if [[ -n $1 ]]; then
-  echo "$1"
-  case $1 in
+  R=$1
+  case $R in
   workflow_dispatch) run_command=build_all ;;
   push) run_command=download_artifacts ;;
   issues) run_command=download_artifacts ;;
   build) run_command=build_all ;;
   esac
 
-  $run_command
-
   if [[ -n $2 ]]; then
+    $run_command
     download_single_issue $2
+  else
+    $run_command
   fi
 fi
