7d2e4c64511be006640989748cceb58eb82ea105
nntrn <17685332+nntrn@users.noreply.github.com>
2024-03-14 00:56:56 -0500

    Update build2.sh

--- scripts/build2.sh
+++ scripts/build2.sh
@@ -4,111 +4,105 @@
 export GITHUB_TOKEN
 TMPDIR=$(mktemp -d)
 
+if [[ -n $GH_TOKEN ]]; then
+  GITHUB_TOKEN=$GH_TOKEN
+fi
+
+_log() { echo -e "\033[0;${2:-33}m$1\033[0m" 3>&2 2>&1 >&3 3>&-; }
+_completed() { _log "Completed: $*" 32; }
+_error() { _log "Error: $*" 31; }
+_info() { _log "Info: $*" 36; }
+
 cleanup() {
   RC=$?
   if [[ $RC -eq 0 ]]; then
     rm -rf $TMPDIR
     [[ -d _data~ ]] && rm -rf _data~
+    _completed "Cleaning up"
   else
-    echo "TMPDIR=$TMPDIR"
+    _error "Did not clean up old directories"
   fi
 }
 
-# trap cleanup INT TERM EXIT
-
-# trap 'rm -rf "$TMPDIR"' EXIT
-
-if [[ -n $GH_TOKEN ]]; then
-  GITHUB_TOKEN=$GH_TOKEN
-fi
-
-_log() { echo -e "\033[0;${2:-33}m$*\033[0m" 3>&2 2>&1 >&3 3>&-; }
-
 ghcurl() {
   local URL="${1:-"https://api.github.com/repos/nntrn/save/issues?per_page=100"}"
-  curl -s -o ${2:-/dev/stdout} -H "Authorization: Bearer $GITHUB_TOKEN" "$URL" --fail
+  curl -L -s -o ${2:-/dev/stdout} -H "Authorization: Bearer $GITHUB_TOKEN" "$URL" --fail
   [[ $? -ne 0 ]] && return 1
 }
 
-jq_filter() {
-  jq 'map(select(.author_association == "OWNER"))|map(.number |= tostring| del(.reactions,.user))'
+download_all_issues() {
+  ghcurl "https://api.github.com/repos/nntrn/save/issues?per_page=100" |
+    jq 'map(select(.author_association == "OWNER")|del(.reactions,.user)|.number |= tostring)'
 }
 
 build_all() {
   _log "Running build_all"
   [[ -d _data ]] && mv _data _data~
   mkdir -p _data/comments
-  ghcurl "https://api.github.com/repos/nntrn/save/issues?per_page=100" |
-    jq 'map(select(.author_association == "OWNER")|del(.reactions,.user)|.number |= tostring)' >_data/issues.json
+  download_all_issues >_data/issues.json
 
-  _log "Creating comments"
-  COMMENTS=($(jq -r 'map(select(.comments > 0)|.number)|join("\n")' _data/issues.json))
-  for issue_id in "${COMMENTS[@]}"; do
-    ghcurl "https://api.github.com/repos/nntrn/save/issues/$issue_id/comments?per_page=100" |
-      jq 'map(select(.author_association == "OWNER")
-      | . + {number: (.issue_url|split("/")|last)}
-      | del(.reactions,.user))' >_data/comments/$issue_id.json
-  done
+  _info "Creating comments"
+  jq -r '.[]|[.comments,.number,.comments_url]|join(" ")' _data/issues.json | awk '$1 > 0' >$TMPDIR/comments.txt
+
+  while read comments number comments_url; do
+    ghcurl "${comments_url}?per_page=100" |
+      jq --arg n $number 'map(select(.author_association == "OWNER") 
+        | . + {number:$n} 
+        | del(.user,.reactions))' >_data/comments/$number.json
+  done <$TMPDIR/comments.txt
 }
 
 download_artifacts() {
   _log "Running download_artifacts"
   ARTIFACTS_FILE=artifacts.json
   ghcurl "https://api.github.com/repos/nntrn/save/actions/artifacts" >$ARTIFACTS_FILE
-  curl -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/nntrn/save/actions/artifacts" >$ARTIFACTS_FILE
 
   LAST_ARCHIVE_DATA_URL="$(
     jq -r '(.artifacts|sort_by(.created_at)|map(select(.name == "page_data" and (.expired|not)).archive_download_url)|last)? // ""' $ARTIFACTS_FILE
   )"
 
   if [[ -n $LAST_ARCHIVE_DATA_URL ]]; then
-    _log "$LAST_ARCHIVE_DATA_URL"
-    ZIPFILENAME=data.zip
-    curl -o $ZIPFILENAME -L -H "Authorization: Bearer $GITHUB_TOKEN" "$LAST_ARCHIVE_DATA_URL"
-    if [[ -d _data ]]; then
-      ls -laptr _data
-      mv _data _data~
-    fi
-    _log "Unzipping"
-    unzip -d _data $ZIPFILENAME
+    _info "Downloading $LAST_ARCHIVE_DATA_URL"
+    ghcurl "$LAST_ARCHIVE_DATA_URL" $TMPDIR/page_data.zip
+    [[ -d _data ]] && mv _data _data~
+    unzip -d _data $TMPDIR/page_data.zip
   else
-    cat $ARTIFACTS_FILE
-    _log "Archive data null"
+    _info "No available artifacts... Building all"
     build_all
   fi
 }
 
-download_single_issue() {
-  issueid=$1
-  _log "Running download_single_issue $issueid"
-  ISSUEFILE=_data/body/${issueid}.json
-  COMMENTSFILE=_data/comments/${issueid}.json
-  mkdir -p _data/{body,comments}
+download_issue() {
+  download_artifacts
+  _log "Running download_issue"
 
-  ghcurl "https://api.github.com/repos/nntrn/save/issues/${issueid}" $ISSUEFILE
-  NUMCOMMENTS=$(jq -r '.comments' $ISSUEFILE)
+  export ISSUE_NUM=$1
+  mkdir -p _data/comments
+  download_all_issues >_data/issues.json
+
+  NUMCOMMENTS=$(jq -r '.[]|select((.number|tonumber) == (env.ISSUE_NUM|tonumber)).comments' _data/issues.json)
+
   if [[ $NUMCOMMENTS -gt 0 ]]; then
-    _log "Writing $COMMENTSFILE"
-    ghcurl "https://api.github.com/repos/nntrn/save/issues/$issueid/comments?per_page=100" >$COMMENTSFILE
+    _info "Fetching comments for $ISSUE_NUM"
+    ghcurl "https://api.github.com/repos/nntrn/save/issues/$ISSUE_NUM/comments?per_page=100" >_data/comments/${ISSUE_NUM}.json
   fi
 }
 
+noop() {
+  set
+}
+
 trap cleanup EXIT
-run_command="build_all"
+run_command="noop"
 
 if [[ -n $1 ]]; then
-  R=$1
-  case $R in
-  workflow_dispatch) run_command=build_all ;;
-  push) run_command=download_artifacts ;;
-  issues) run_command=download_artifacts ;;
-  build) run_command=build_all ;;
+  case $1 in
+  workflow*) run_command="build_all" ;;
+  push) run_command="download_artifacts" ;;
+  issue*) run_command="download_issue $2" ;;
+  build) run_command="build_all" ;;
   esac
 
-  if [[ -n $2 ]]; then
-    $run_command
-    download_single_issue $2
-  else
-    $run_command
-  fi
+  $run_command
+
 fi
