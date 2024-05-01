bf352c27f147a34d145706faffd832e311fce967
nntrn <17685332+nntrn@users.noreply.github.com>
2024-05-01 13:54:49 -0500

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,69 +1,60 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
+
 export GITHUB_TOKEN
-TMPDIR=$(mktemp -d)
-trap 'rm -rf "$TMPDIR"' EXIT
+export ISSUES_API_URL='https://api.github.com/repos/nntrn/save/issues?per_page=100&state=open'
 
-if [[ -z $GITHUB_TOKEN ]]; then
-  GITHUB_TOKEN=$GH_TOKEN
-fi
+GITHUB_TOKEN=${GITHUB_TOKEN:-$GH_TOKEN}
 
-_log() { echo -e "\033[0;${2:-33}m$*\033[0m" 3>&2 2>&1 >&3 3>&-; }
+_log() { echo -e "\033[0;${2:-33}m$1\033[0m" 3>&2 2>&1 >&3 3>&-; }
 
 ghcurl() {
-  local URL="${1:-"https://api.github.com/repos/nntrn/save/issues?per_page=100"}"
-  curl -s -o ${2:-/dev/stdout} "$URL" -H "Authorization: Bearer $GITHUB_TOKEN" --fail
-  [[ $? -ne 0 ]] && exit 1
+  local URL="${1}"
+  local OUTFILE="${2}"
+  _log "$URL"
+  curl -L -s --create-dirs -o $OUTFILE \
+    -H "Authorization: Bearer $GITHUB_TOKEN" "$URL" --fail
+  # [[ $? -ne 0 ]] && return 1
+  # [[ -n $2 && ! -f $2 ]] && return 1
+  # [[ -f $2 && ! -s $2 ]] && return 1
 }
 
-jq_filter() {
-  jq 'map(select(.author_association == "OWNER"))|map(.number |= tostring| del(.reactions,.user))'
-}
+download_all_issues() {
+  _len=100
+  _page=0
 
-download_artifacts() {
-  _log "Running download_artifacts" 36
-  ARTIFACTS_URL=https://api.github.com/repos/nntrn/save/actions/artifacts
-  ghcurl "https://api.github.com/repos/nntrn/save/actions/artifacts" $TMPDIR/artifacts.json
+  while test $_len -eq 100; do
+    _page=$((_page + 1))
+    _log $_page
+    _out="$_tmp/issue-${_page}.json"
 
-  LAST_ARCHIVE_DATA_URL="$(
-    curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$ARTIFACTS_URL" |
-      jq -r '.artifacts[]|select(.name == "page_data" and (.expired|not)).archive_download_url? // ""'
-  )"
-
-  if [[ -n $LAST_ARCHIVE_DATA_URL ]]; then
-    curl -o data.zip -L -H "Authorization: Bearer $GITHUB_TOKEN" "$LAST_ARCHIVE_DATA_URL"
-    unzip data.zip
-    mkdir _data
-    tar -xf artifact.tar -C _data
-  else
-    exit 1
-  fi
-}
-
-build_all() {
-  _log "Running build_all" 36
-  ISSUESFILE=_data/issues.json
-  mkdir -p _data/{body,comments}
-  ghcurl "https://api.github.com/repos/nntrn/save/issues?per_page=100" | jq_filter >$ISSUESFILE
-  _log "Creating comments"
-  COMMENTS=($(jq -r '.[]|select(.comments > 0)|.number' $ISSUESFILE))
-  for issue_id in "${COMMENTS[@]}"; do
-    ghcurl "https://api.github.com/repos/nntrn/save/issues/$issue_id/comments?per_page=100" |
-      jq_filter >_data/comments/$issue_id.json
+    ghcurl "${ISSUES_API_URL}&page=${_page}" "$_out"
+    _len=$(jq -r 'length' $_out)
+    _log "$_len"
   done
 }
 
-env
-run_command=build_all
+build_all() {
+  mkdir -p _data
+  download_all_issues
 
-if [[ -n $1 ]]; then
-  case $1 in
-  workflow_dispatch) run_command=build_all ;;
-  push*) run_command=download_artifacts ;;
-  issue*) run_command=download_artifacts ;;
-  esac
-fi
+  cat $_tmp/issue*.json | jq -s 'flatten' |
+    jq 'map(select(.author_association == "OWNER") | 
+      del(.reactions,.user)|.number |= tostring)' >_data/issues.json
 
-echo "Running $run_command"
-$run_command
+  jq -r '.[]|[.comments,.number,.comments_url]|join(" ")' _data/issues.json |
+    awk '$1 > 0' >$_tmp/comments.txt
+
+  while read c id comments_url; do
+    ghcurl "$comments_url" "$_tmp/comments-${id}.json"
+  done <$_tmp/comments.txt
+
+  jq -s 'flatten | map(select(.author_association == "OWNER") |
+    . + {number: (.issue_url|split("/")|last)}|del(.user,.reactions)
+    )' .cache/comments*.json >_data/comments.json
+}
+
+_tmp=$(mktemp -d)
+[[ -d _data ]] && rm -rf _data
+build_all
