216a39e7aa38e817d37d9e961ab92189f44a13d8
nntrn <17685332+nntrn@users.noreply.github.com>
2024-05-01 14:13:33 -0500

    Fix download

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,38 +23,39 @@ ghcurl() {
 download_all_issues() {
   _len=100
   _page=0
-
+  mkdir -p _tmp
   while test $_len -eq 100; do
     _page=$((_page + 1))
     _log $_page
-    _out="$_tmp/issue-${_page}.json"
+    _out=_tmp/issue-${_page}.json
 
     ghcurl "${ISSUES_API_URL}&page=${_page}" "$_out"
     _len=$(jq -r 'length' $_out)
     _log "$_len"
   done
+
+  jq -s 'flatten' _tmp/issue*.json |
+    jq 'map(select(.author_association == "OWNER") | 
+      del(.reactions,.user)|.number |= tostring)'
 }
 
 build_all() {
-  mkdir -p _data
-  download_all_issues
+  mkdir -p _tmp
+  [[ -d _data ]] && rm -rf _data
 
-  cat $_tmp/issue*.json | jq -s 'flatten' |
-    jq 'map(select(.author_association == "OWNER") | 
-      del(.reactions,.user)|.number |= tostring)' >_data/issues.json
+  mkdir -p _data
+  download_all_issues >_data/issues.json
 
   jq -r '.[]|[.comments,.number,.comments_url]|join(" ")' _data/issues.json |
-    awk '$1 > 0' >$_tmp/comments.txt
+    awk '$1 > 0' >_tmp/comments.txt
 
-  while read c id comments_url; do
-    ghcurl "$comments_url" "$_tmp/comments-${id}.json"
-  done <$_tmp/comments.txt
+  while read c number comments_url; do
+    ghcurl $comments_url _tmp/comments-$number.json
+  done <_tmp/comments.txt
 
   jq -s 'flatten | map(select(.author_association == "OWNER") |
     . + {number: (.issue_url|split("/")|last)}|del(.user,.reactions)
-    )' .cache/comments*.json >_data/comments.json
+    )' _tmp/comments*.json >_data/comments.json
 }
 
-_tmp=$(mktemp -d)
-[[ -d _data ]] && rm -rf _data
 build_all
