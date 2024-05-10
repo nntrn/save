1c0e6ba7da80a280074ecd7c941c0a0584fde2b6
nntrn <17685332+nntrn@users.noreply.github.com>
2024-05-10 00:33:00 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-SCRIPT="$(realpath "$0")"
+set -e
+
+SCRIPT="$(realpath $0)"
 DIR=${SCRIPT%/*/*}
 DATADIR=$DIR/_data
+
+echo $DATADIR
 
 export GITHUB_TOKEN
 
@@ -10,32 +14,40 @@ ISSUES_API_URL='https://api.github.com/repos/nntrn/save/issues?per_page=100&stat
 JQ_CLEAN_EXPR='map(select(.author_association == "OWNER")|del(.reactions,.user)|.+{number:((.issue_url? // .url)|split("/")|last)})'
 JQ_COMMENTS_EXPR='.[]|select(.comments > 0)|[(.url|split("/")|last)+".json",.comments_url]|join(" ")'
 
-# export OUTDIR=${1:-$PWD/_data}
-
 if [[ -n $GH_TOKEN ]]; then
   GITHUB_TOKEN=$GH_TOKEN
 fi
 
 _log() { echo -e "\033[0;${2:-33}m$1\033[0m" 3>&2 2>&1 >&3 3>&-; }
+_exitMsg() {
+  _log "$1" 31
+  exit 1
+}
+
+_hash() {
+  local YMD,CHKSUM
+  YMD=$(date +%Y%m%d)
+  CHKSUM=$(cksum <<<'hello world')
+  echo "${CHKSUM% *}.${YMD}"
+}
 
 _gh() {
+  [[ -z $GITHUB_TOKEN ]] && _exitMsg "GITHUB_TOKEN is empty"
   curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$1" --fail
+  [[ $? -ne 0 ]] && _exitMsg "An error occured fetching"
 }
 
 _main() {
-  OUTDIR="$(realpath $1)"
-
+  OUTDIR=$1
   [[ -d $OUTDIR ]] && rm -rf $OUTDIR
-
   mkdir -p $OUTDIR/.comment
   _gh "$ISSUES_API_URL" | jq "$JQ_CLEAN_EXPR" >$OUTDIR/issues.json
-
   while read _path _url; do
     echo "$_url"
     _gh "$_url" | jq "$JQ_CLEAN_EXPR" >$OUTDIR/.comment/$_path
   done < <(jq -r "$JQ_COMMENTS_EXPR" $OUTDIR/issues.json)
-
   jq -s 'flatten' $OUTDIR/.comment/*.json >$OUTDIR/comments.json
+  echo '*' >$OUTDIR/.gitignore
   echo "$OUTDIR"
 }
 
