961142e168b4dd56c75b5916ade33bf3c33c8bb3
nntrn <17685332+nntrn@users.noreply.github.com>
2024-05-12 22:19:19 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,9 +6,6 @@ SCRIPT="$(realpath $0)"
 DIR=${SCRIPT%/*/*}
 ISSUES_API_URL='https://api.github.com/repos/nntrn/save/issues?per_page=100&state=open'
 
-[[ -n $GH_TOKEN ]] &&
-  GITHUB_TOKEN=$GH_TOKEN
-
 JQ_CLEAN_EXPR='map(
   select(.author_association == "OWNER")
   | del(.reactions,.user)
@@ -20,18 +17,24 @@ JQ_COMMENTS_EXPR='.[]
   | [(.url|split("/")|last)+".json",.comments_url]
   | join(" ")'
 
-_log() { echo -e "\033[0;${2:-33}m$1\033[0m" 3>&2 2>&1 >&3 3>&-; }
+LABEL_SUCCESS='\033[0;32m✔ \033[0m'
+LABEL_ERROR='\033[0;31m✘ \033[0m'
+
+_log() { echo -e "\033[0;${2:-37}m$1\033[0m" 3>&2 2>&1 >&3 3>&-; }
 _exitMsg() { _log "$1" 31 && exit 1; }
 
-_hash() {
+check_file() { [[ -f $1 && -s $1 ]] && _log "$LABEL_SUCCESS $1" || _exitMsg "$LABEL_ERROR $1"; }
+
+_chksum() {
   local YMD,CHKSUM
   YMD=$(date +%Y%m%d)
-  CHKSUM=$(cksum <<<'hello world')
+  CHKSUM=$(cksum <<<"$@")
   echo "${CHKSUM% *}.${YMD}"
 }
 
 _gh() {
   [[ -z $GITHUB_TOKEN ]] && _exitMsg "GITHUB_TOKEN is empty"
+  _log "$1"
   curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$1" --fail
   [[ $? -ne 0 ]] && _exitMsg "An error occured fetching"
 }
@@ -41,15 +44,18 @@ _main() {
   [[ -d $OUTDIR/.comment ]] && rm -rf $OUTDIR/.comment
   mkdir -p $OUTDIR/.comment
   _gh "$ISSUES_API_URL" | jq "$JQ_CLEAN_EXPR" >$OUTDIR/issues.json
+  check_file $OUTDIR/issues.json
   while read _path _url; do
-    echo "$_url"
     _gh "$_url" | jq "$JQ_CLEAN_EXPR" >$OUTDIR/.comment/$_path
+    check_file $OUTDIR/.comment/$_path
   done < <(jq -r "$JQ_COMMENTS_EXPR" $OUTDIR/issues.json)
   jq -s 'flatten' $OUTDIR/.comment/*.json >$OUTDIR/comments.json
+  check_file $OUTDIR/comments.json
   echo '*' >$OUTDIR/.gitignore
-  echo "$OUTDIR"
 }
 
-_log "Output: $DIR/_data"
+[[ -n $GH_TOKEN ]] &&
+  GITHUB_TOKEN=$GH_TOKEN
 
+# _log "Output: ${DIR/$PWD\//.}/_data"
 _main ${1:-$DIR/_data}
